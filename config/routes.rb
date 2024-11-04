58623aef841e1c5b4fd507c492410578b8b82a4d
atauber27 <123479989+atauber27@users.noreply.github.com>
2024-11-03 19:56:49 -0500

    boilerplate home page (#41)
    * boilerplate home page
    
    * Buttons fixed and styling added
    
    ---------
    
    Co-authored-by: Eli <eheakins@colgate.edu>

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   resources :tags
+  resources :home, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,5 +15,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#show"
 end
