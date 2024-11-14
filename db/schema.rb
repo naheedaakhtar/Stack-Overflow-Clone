9c659c95b77006451242f7ac87ab7b1048163eb9
Eliheakins <123508908+Eliheakins@users.noreply.github.com>
2024-11-14 18:01:27 -0500

    54 reimplement user css (#55)
    * Bug fix with fields for new user
    
    * Css updates
    
    ---------
    
    Co-authored-by: Eli <eheakins@colgate.edu>

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_04_210247) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
