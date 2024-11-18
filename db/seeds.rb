05c1d8404016cdb021f4a7b2d1a1a4b30cc21da7
JacobKrehbiel03 <143643828+JacobKrehbiel03@users.noreply.github.com>
2024-11-17 20:36:11 -0500

    Added many seeds for tags (#60)

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,18 @@
   User.create!(username: 'admin', password: 'password', firstname: 'admin', lastname: 'admin', email: 'admin@colgate.edu', role: 'admin')
 
   Tag.create!(name: "", description: "Empty Tag")
+  Tag.create!(name: "Python", description: "Python is a programming language that allows for many different programming practices including Object Oriented Programming and many machine learning packages ")
+  Tag.create!(name: "Java", description: "Java is an programming languages with static typing to create code that can be ran ony any platform once completed")
+  Tag.create!(name: "C", description: "C is a programming language that allows for specific interactions with the low level processes of programming")
+  Tag.create!(name: "Ruby", description: "Ruby is a programming language designed to reduce confusion by following consistent procedures and giving freedom to programmers")
+  Tag.create!(name: "Ruby on Rails", description: "Ruby on Rails is a series of packages for Ruby used for Software Engineering and Agile Programming")
+  Tag.create!(name: "SQL", description:"SQL is a language used to analyze and work with databases")
+  Tag.create!(name: "R", description: "R is a programming language primarily used for statistical purposes" )
+  Tag.create!(name: "Javascript", description: "Javascript is a language used for webpage user interaction purposes and other complicated website interface code" )
+  Tag.create!(name: "Git", description: "Git is a method for teams of programmers to work with branchs, version control, and working apart")
+
+  Tag.create!(name: "Design Principles", description: "Questions that are asking about the why behind design decisions")
+  Tag.create!(name: "Compilation Error", description: "Questions asking about errors with programs compiling")
+  Tag.create!(name: "Semantic Error", description: "Questions asking about programs producing unexpected output")
+  Tag.create!(name: "Runtime Error", description: "Questions asking about programs producing errors at runtime")
+
